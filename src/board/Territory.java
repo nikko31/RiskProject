751398ef31e17615fc04d1499431a9ad7b6b729e
FEDE <f.fulgeri@gmail.com>
2015-11-10 20:09:17 +0100

    ho iniziato un po' a scrivere i metodi per i continenti i territori e dadi giusto per vedere se sul pc fisso funzionava.

--- src/board/Territory.java
+++ src/board/Territory.java
@@ -1,7 +1,109 @@
 package board;
 
+import java.awt.*;
+import java.util.ArrayList;
+
 /**
  * Created by DarkLinux on 09/11/15.
  */
 public interface Territory {
+
+    /**
+     * set territory name
+     * @param name
+     * name of territory
+     */
+    void setName(String name);
+
+
+    /**
+     * get territory name
+     * @return
+     * name of territory
+     */
+    String getName();
+
+
+    /**
+     * set the owner
+     * @param owner
+     * player owner
+     *
+     * PER ADESSO HO MESSO COSI
+     * POI NON SO SE FAREMO CON UN SEMPLICE ID NUMERICO
+     * O SE LO TOGLIEREMO !!!!!!NB LE COMMENTO PERCHE DANNO ERRORE
+     * NON ESSENDO IMPLEMTNETATO IL PLAYER
+     *
+     */
+    //void setOwner(Player owner);
+
+    /**
+     * territory owner
+     * @return
+     * owner of territory
+     */
+    //Player getOwner();
+
+    /**
+     * set the troops
+     * on this territory
+     * @param troops
+     * number of troops
+     */
+    void setTroopsOn (int troops);
+
+    /**
+     * number of troops
+     * on this territory
+     * @return
+     * number of troops
+     */
+    int getTroopsOn ();
+
+    /**
+     * set territory
+     * color
+     * @param color
+     * color
+     * to use
+     * NON SO SE SERVE...MA COMUNQUE
+     * CE LA METTO AL MASSIMO SI  TOLGONO
+     * SIA LA SET CHE LA GET
+     */
+    void setColor(Color color);
+
+    /**
+     * get terrytory color
+     * @return
+     * color
+     */
+     Color getColor();
+
+
+    /**
+     * all the border
+     * territories
+     * @return
+     * list of border territories
+     */
+    ArrayList<Territory> getBorder();
+
+    /**
+     * add a border territory
+     * @param territory
+     * territory to add at
+     * the border list
+     */
+    void addBorder(Territory territory);
+
+
+    /**
+     * check if the territory
+     * is neighboring
+     * @return
+     * true in positive case
+     * otherwise false
+     */
+    boolean isBorder();
+
 }
