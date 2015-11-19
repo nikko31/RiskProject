d05f4303dd978c910ca78dffdc52ab17557dea38
nikko31 <nikkofer31@gmail.com>
2015-11-19 09:47:59 +0000

    DONE:Territory class

--- src/risk/GameResources.java
+++ src/risk/GameResources.java
@@ -2,9 +2,6 @@ package risk;
 
 import java.util.*;
 
-/**
- * Created by DarkLinux on 19/11/15.
- */
 public class GameResources {
     public enum COLOR {RED, YELLOW, GREEN, BLUE, VIOLET, BROWN}
 
