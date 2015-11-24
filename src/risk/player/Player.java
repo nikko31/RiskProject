0aabf98ef06f872b19687c16f8240a56e393b3c3
nikko31 <nikkofer31@gmail.com>
2015-11-24 16:14:41 +0000

    commento inutile

--- src/risk/player/Player.java
+++ src/risk/player/Player.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+/**/
 public class Player {
     private int playerID;
     private String playerName;
