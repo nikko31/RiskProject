20813cc2ea1525aadb526301fef7bb633c6bb16e
Federico <f.fulgeri@gmail.com>
2015-11-30 10:17:12 +0100

    aggiunta carteal giocatore

--- src/risk/player/Player.java
+++ src/risk/player/Player.java
@@ -1,6 +1,8 @@
 package risk.player;
 
 
+import risk.board.Card;
+
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -12,7 +14,7 @@ public class Player {
     private String playerName;
     private int freeUnits;
     private Color playerColor;
-
+    private List<Card> cards;
     private List<String> continentList;
 
     public Player(int playerID, String playerName, int freeUnits, Color color) {
@@ -20,6 +22,7 @@ public class Player {
         this.playerName = playerName;
         this.playerColor = color;
         this.freeUnits = freeUnits;
+        this.cards = new ArrayList<>();
         this.continentList = new ArrayList<>();
     }
 
@@ -52,5 +55,23 @@ public class Player {
 
     public void setContinentList(List<String> continentList) {
         this.continentList = continentList;
+    }
+
+    public void addPlayerCard(Card card){
+        cards.add(card);
+    }
+
+    public void removePlayerCard(List<Card> cardsToRemove ){
+        for(Card carditer : cardsToRemove){
+            cards.remove(carditer);
+        }
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public void setCards(List<Card> cards) {
+        this.cards = cards;
     }
 }
