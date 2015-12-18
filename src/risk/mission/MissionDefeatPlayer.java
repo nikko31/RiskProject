8d915664323ca7cacc2b53b8e58e416bdff16b1a
Federico <f.fulgeri@gmail.com>
2015-12-18 10:34:12 +0100

    all mission seems work

--- src/risk/mission/MissionDefeatPlayer.java
+++ src/risk/mission/MissionDefeatPlayer.java
@@ -3,9 +3,7 @@ package risk.mission;
 import risk.GameState;
 import risk.board.Territory;
 import risk.player.Player;
-
 import java.awt.*;
-import java.util.Map;
 
 /**
  * Created by Federico on 17/12/2015.
@@ -13,32 +11,36 @@ import java.util.Map;
 public class MissionDefeatPlayer implements Mission {
     private Color playerToDef;
     private boolean firstcheck;
+    private boolean choiche;
 
     public MissionDefeatPlayer(Color playerToDef) {
         this.firstcheck = true;
         this.playerToDef = playerToDef;
+        this.choiche = true;
     }
 
     @Override
     public boolean checkHitMission(GameState gameState, Player playerCurrent) {
-        System.out.println("controllo missione sconfiggi");
+        System.out.println("checkDefeatMission");
         int counter = 0;
 
         if(firstcheck){
             for(Player player : gameState.getPlayers()){
                 if (player.getPlayerColor().equals(playerToDef)
                         && !gameState.getCurrentPlayerTurn().getPlayerColor().equals(playerToDef)){
-                    System.out.println("c'è giocatore avversario con colore richiesto");
-                    firstcheck = false;
+                    System.out.println("there is one player with the request color");
+                    choiche = false;
                     return false;
                 }
 
             }
+            firstcheck = false;
+
 
         }
-        if(firstcheck){
+        if(choiche){
+            System.out.println("you can't self destroy: conquest 24 territories ");
             for (Territory territory : gameState.getTerritoriesPlayersMap().keySet()) {
-                System.out.println("tu hai quel colore allora conquista i territory");
                 if (gameState.getPlayerTer(territory).equals(gameState.getCurrentPlayerTurn())) {
                     counter++;
                 }
@@ -51,13 +53,13 @@ public class MissionDefeatPlayer implements Mission {
         }
         else{
             if(gameState.getPlayerEliminated().get(playerToDef) != null){
-                System.out.println("c'è giocatore avversario con colore richiesto");
+                System.out.println("there is a player with the color you search");
                 if(gameState.getPlayerEliminated().get(playerToDef).equals(gameState.getCurrentPlayerTurn())){
-                    System.out.println("c'è giocatore avversario con colore richiesto e l ho eliminato io");
+                    System.out.println("i eliminate the player");
                     return true;
                 }
                 else{
-                    System.out.println("c'è giocatore avversario con colore richiesto e non l'ho eliminato io");
+                    System.out.println("someone eliminate my objective");
                     for (Territory territory : gameState.getTerritoriesPlayersMap().keySet()) {
                         if (gameState.getPlayerTer(territory).equals(gameState.getCurrentPlayerTurn())) {
                             counter++;
@@ -77,37 +79,39 @@ public class MissionDefeatPlayer implements Mission {
 
     }
 
-    /*
-    MISSION_DESTROY.put(0,Color.RED);
-    MISSION_DESTROY.put(1,Color.cyan);
-    MISSION_DESTROY.put(2,Color.decode("#FF33FF"));
-    MISSION_DESTROY.put(3,Color.decode("#00FF00"));
-    MISSION_DESTROY.put(4,Color.decode("#0000FF"));
-    MISSION_DESTROY.put(5,Color.yellow);
-    */
-    /*
+
     private String colorToString(Color color){
-        switch(color.getRGB()){
-            case  16711680:
-                return "RED";
-            case 65280:
-                return "GREEN";
-            case :
-                retrun "";
-            case 255
-            retrun " BLUE";
-            case :
-            retrun "";
-            case :
-            retrun "";
+
+        if(color.equals(Color.RED)){
+            return "RED";
         }
+        else if(color.equals(Color.cyan)){
+            return "CYAN";
+        }
+        else if(color.equals(Color.yellow)){
+            return "YELLOW";
+        }
+        else if(color.equals(Color.decode("#FF33FF"))){
+            return "PURPLE";
+        }
+        else if(color.equals(Color.decode("#00FF00"))){
+            return "GREEN";
+        }
+        else if(color.equals(Color.decode("#0000FF"))){
+            return "BLUE";
+        }
+        else {
+            return "error";
+        }
+
+
     }
-    */
+
 
 
     @Override
     public String toString() {
-        return "you have to defeat: " + " " + this.playerToDef.toString() + " if is your color or someone defeat him," +
+        return "you have to defeat: " + " " + this.playerToDef.toString() +" "+ colorToString(playerToDef) + " player, if is your color or someone defeat him," +
                 " you have to conquer 24 territories ";
     }
 
